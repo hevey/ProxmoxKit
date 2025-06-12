0e363339e19546653efe7ff43dc0cd06a38b136d
Glenn Hevey <glenn.hevey@gmail.com>
2025-06-12 20:48:01 +1000

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ProxmoxKit",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ProxmoxKit",
+            targets: ["ProxmoxKit"]
+        ),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "ProxmoxKit"
+        ),
+        .testTarget(
+            name: "ProxmoxKitTests",
+            dependencies: ["ProxmoxKit"]
+        ),
+    ]
+)
